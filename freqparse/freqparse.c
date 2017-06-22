d8901c743001daa31a6e9351a663d98bd51c8d1d
admin <admin@localhost.localdomain>
2017-06-21 18:07:41 -0700

    Day 3

--- freqparse/freqparse.c
+++ freqparse/freqparse.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 int main() {
   // initialize variables;
-
+  int counter, digits, lowerc, other;
   char c;
-
+  counter = 50; //checking first 50, need to change
+  digits = lowerc = other = 0;
   while ((c = getchar()) != EOF) {
     // process one character
+    if (c >= '0' && c <= '9') {
+      digits++;
+    }
+    if (c >= 'a' && c <= 'z') {
+      lowerc++;
+    }
+    else {
+      other++;
+    }
+
   }
 
   // Output results here
